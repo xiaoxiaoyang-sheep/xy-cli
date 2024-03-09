039981df53dc8f7ee2643b9341f9a464c3ddc739
xy <904852749@qq.com>
2024-03-09 21:18:08 +0800

    1

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'rollup';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import {externals} from "rollup-plugin-node-externals";
+import externals from "rollup-plugin-node-externals";
 import json from "@rollup/plugin-json";
 import terser from "@rollup/plugin-terser";
 import typescript from 'rollup-plugin-typescript2';
